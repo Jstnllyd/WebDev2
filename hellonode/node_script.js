796cb315e76993ca7d3f3ef9ecfa637b61c0a0bb
Jstnllyd <palma.it111@gmail.com>
2020-01-28 11:39:48 +0800

    first commit

---
+++ hellonode/node_script.js
@@ -0,0 +1,14 @@
+class A{
+        constructor(){
+            console.log("Constructior");
+            
+        }
+        getMessage(){
+            console.log("Hello Node");
+
+        }
+        
+}
+const a = new A()
+a.getMessage()
+    
